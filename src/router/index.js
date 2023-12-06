6c83ba2490d04b36904020eb0a7816ef73b15ba4
takahashi.yunosuke <takahashi.yunosuke.cr9@jp.nssol.nipponsteel.com>
2023-12-06 16:32:41 +0900

    db.json試し

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/TestView.vue')
+  },
+  {
+    path: '/booklist',
+    name: 'booklist',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/BookList.vue')
   }
 ]
 
