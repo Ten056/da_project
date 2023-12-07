dea250fe95dc1209a8d67dde9ff0ae175121ac3a
takahashi.yunosuke <takahashi.yunosuke.cr9@jp.nssol.nipponsteel.com>
2023-12-07 17:41:56 +0900

    予約済み図書一覧作成完了

--- src/views/BookList.vue
+++ src/views/BookList.vue
@@ -5,6 +5,7 @@ import axios from 'axios'
 const todoData = ref(null)
 
 function getTodo() {
+  // axiosを利用し、サーバーよりデータを取得
   const url = `http://localhost:3000/books`
   todoData.value = null
   axios
@@ -14,16 +15,77 @@ function getTodo() {
 }
 getTodo()
 
-// export default {
-//     name: 'BookList',
-// }
 </script>
 
 <template>
   <div class="booklist">
-    <h1>booklist</h1>
-    <p v-if="!todoData">Loading...</p>
-    <pre v-else v-for="data in todoData" v-bind:key="data">{{ data.id }}</pre>
+    <h1>予約済み図書一覧</h1>
+    <div id="tmp">
+      <select><option>すべて</option></select>
     </div>
+    <div id="mainContents">
+      <table>
+        <thead><tr>
+          <th id="icon">アイコン</th>
+          <th id="title">タイトル</th>
+          <th id="status">ステータス</th>
+        </tr></thead>
+
+        <tbody>
+          <p v-if="!todoData">Loading...</p>
+          <tr v-else v-for="data in todoData" v-bind:key="data">
+            <td><img v-bind:src='data.id + ".png"' alt="icon"></td>
+            <td>{{ data.title }}</td>
+            <td>{{ data.status }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
-  
+
+
+<style>
+div#mainContents table{
+  margin:0 auto 0 auto;
+  width:800px;
+}
+
+div#mainContents table th{
+  background-color: rgb(229, 235, 240);
+}
+
+  div#mainContents table th#icon{
+  border:solid;
+  width:50px;
+}
+
+div#mainContents table th#title{
+  border:solid;
+  width:80px;
+}
+
+
+div#mainContents table th#status{
+  border:solid;
+  width:30px;
+}
+
+select{
+  position:relative;
+  left:-365px;
+  margin-bottom: 2px;
+  font-size:15px;
+
+}
+
+img{
+  width: 20px;
+  height: 20px;
+}
+
+/* table{
+	table-layout: fixed;
+	width: 100%;
+}	 */
+</style>
