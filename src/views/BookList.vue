b4c9c396f5a516a833651b5ca525c0f1e8d87a46
takahashi.yunosuke <takahashi.yunosuke.cr9@jp.nssol.nipponsteel.com>
2023-12-07 10:56:48 +0900

    BookListVueで、一覧表示を作成中

--- src/views/BookList.vue
+++ src/views/BookList.vue
@@ -1,14 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+const todoData = ref(null)
+
+function getTodo() {
+  const url = `http://localhost:3000/books`
+  todoData.value = null
+  axios
+    .get(url)
+    .then((res) => todoData.value = res.data)
+    .catch((error) => todoData.value = error)
+}
+getTodo()
+
+// export default {
+//     name: 'BookList',
+// }
+</script>
+
 <template>
-    <div class="booklist">
-      <p>booklist</p>
+  <div class="booklist">
+    <h1>booklist</h1>
+    <p v-if="!todoData">Loading...</p>
+    <pre v-else v-for="data in todoData" v-bind:key="data">{{ data.id }}</pre>
     </div>
-  </template>
-  
-  <script>
-  // @ is an alias to /src
-  
-//   export default {
-//     name: 'booklist',
-//   }
-//   </script>
+</template>
   
