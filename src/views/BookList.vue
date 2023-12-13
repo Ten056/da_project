5d1e706cd8885c9f5d4331226bead029d795e168
takahashi.yunosuke <takahashi.yunosuke.cr9@jp.nssol.nipponsteel.com>
2023-12-13 15:58:48 +0900

    読了済みの本を一覧に表示されないように実装。一覧に読了予定日の項目を追加。

--- src/views/BookList.vue
+++ src/views/BookList.vue
@@ -6,28 +6,49 @@
     </div>
     <div id="mainContents">
       <table>
-        <thead><tr>
-          <th id="icon" @click="sortBy('id')" v-bind:class="{ 'sort-asc': sortKey === 'id' && sortOrder === 1, 'sort-desc': sortKey === 'id' && sortOrder === -1 }">アイコン</th>
-          <th id="title" @click="sortBy('title')" v-bind:class="{ 'sort-asc': sortKey === 'title' && sortOrder === 1, 'sort-desc': sortKey === 'title' && sortOrder === -1 }">タイトル</th>
-          <th id="status" @click="sortBy('status')" v-bind:class="{ 'sort-asc': sortKey === 'status' && sortOrder === 1, 'sort-desc': sortKey === 'status' && sortOrder === -1 }">計画作成状況</th>
-        </tr></thead>
+        <thead>
+          <tr>
+            <th id="icon" @click="sortBy('id')"
+              v-bind:class="{ 'sort-asc': sortKey === 'id' && sortOrder === 1, 'sort-desc': sortKey === 'id' && sortOrder === -1 }">
+              アイコン</th>
+            <th id="title" @click="sortBy('title')"
+              v-bind:class="{ 'sort-asc': sortKey === 'title' && sortOrder === 1, 'sort-desc': sortKey === 'title' && sortOrder === -1 }">
+              タイトル</th>
+            <th id="status" @click="sortBy('status')"
+              v-bind:class="{ 'sort-asc': sortKey === 'status' && sortOrder === 1, 'sort-desc': sortKey === 'status' && sortOrder === -1 }">
+              計画作成状況</th>
+            <th id="status" @click="sortBy('status')"
+              v-bind:class="{ 'sort-asc': sortKey === 'deadline' && sortOrder === 1, 'sort-desc': sortKey === 'deadline' && sortOrder === -1 }">
+              読了予定日</th>
+          </tr>
+        </thead>
 
         <tbody>
           <p v-if="!todoData">Loading...</p>
           <tr v-else v-for="data in sortedUsers" v-bind:key="data">
             <td>
-              <router-link v-if="data.status !== -1"  :to="`./bookInfo/${data.id}`"><img v-bind:src='data.thumbnail' alt="icon"></router-link>
-              <router-link v-if="data.status === -1"  :to="`./bookInfo/${data.id}/${data.status}`"><img v-bind:src='data.thumbnail' alt="icon"></router-link>
+              <router-link v-if="data.status !== -1" :to="`./bookInfo/${data.id}`"><img v-bind:src='data.thumbnail'
+                  alt="icon"></router-link>
+              <router-link v-if="data.status === -1" :to="`./bookInfo/${data.id}/${data.status}`"><img
+                  v-bind:src='data.thumbnail' alt="icon"></router-link>
             </td>
             <td>
               <router-link v-if="data.status !== -1" :to="`./bookInfo/${data.id}`">{{ data.title }}</router-link>
-              <router-link v-if="data.status === -1" :to="`./bookInfo/${data.id}/${data.status}`">{{ data.title }}</router-link>
+              <router-link v-if="data.status === -1" :to="`./bookInfo/${data.id}/${data.status}`">{{ data.title
+              }}</router-link>
             </td>
             <td>
-              <router-link v-if="(data.status !== -1)&(data.total_page !== data.status)" :to="`./bookInfo/${data.id}`">in-progress</router-link>
-              <router-link v-if="(data.status !== -1)&(data.total_page === data.status)" :to="`./bookInfo/${data.id}`">finish</router-link>
-              <router-link v-if="data.status === -1" :to="`./bookInfo/${data.id}/${data.status}`">unscheduled</router-link>
+              <router-link v-if="(data.status !== -1) & (data.total_page !== data.status)"
+                :to="`./bookInfo/${data.id}`">進行中</router-link>
+              <router-link v-if="(data.status !== -1) & (data.total_page === data.status)"
+                :to="`./bookInfo/${data.id}`">読了済み</router-link>
+              <router-link v-if="data.status === -1" :to="`./bookInfo/${data.id}/${data.status}`">計画未作成</router-link>
 
+            </td>
+
+            <td>
+              <router-link v-if="data.status !== -1" :to="`./bookInfo/${data.id}`">{{ data.deadline }}</router-link>
+              <router-link v-if="data.status === -1" :to="`./bookInfo/${data.id}/${data.status}`"> </router-link>
             </td>
           </tr>
         </tbody>
@@ -38,55 +59,68 @@
 </template>
 
 <script>
-  import axios from 'axios'
+import axios from 'axios'
 
-  export default {
-    name: "test_page",
-    data: () => ({
-      todoData: "",
-      sortKey: '',
-      sortOrder: 1,
-    }),
-    mounted() {
-      axios.get("http://localhost:3000/books")
-      .then(response => this.todoData = response.data)
+export default {
+  name: "test_page",
+  data: () => ({
+    todoData: [],
+    sortKey: '',
+    sortOrder: 1,
+    isDanger: false,
+
+  }),
+  mounted() {
+    axios.get("http://localhost:3000/books")
+      .then(response => {
+        let tmpData = response.data
+        for (let i = 0; i < tmpData.length; i++) {
+          if (tmpData[i].status === -1 || tmpData[i].total_page != tmpData[i].status)
+            this.todoData.push(tmpData[i])
+          // console.log(tmpData[i])
+        }
+        console.log(this.todoData)
+
+
+      })
       .catch(error => console.log(error))
-      // axios.get("http://localhost:3000/headers")
-      // .then(response => this.headers = response.data)
-      // .catch(error => console.log(error))
-    },
-    computed: {
-      sortedUsers() {
-        const todoData_tmp = [...this.todoData];
-        const sortKey = this.sortKey;
-        const sortOrder = this.sortOrder;
+    // axios.get("http://localhost:3000/headers")
+    // .then(response => this.headers = response.data)
+    // .catch(error => console.log(error))
 
-        if (sortKey) {
-          todoData_tmp.sort((a, b) => {
-            const modifier = sortOrder === 1 ? 1 : -1;
-            if (a[sortKey] < b[sortKey]) return -1 * modifier;
-            if (a[sortKey] > b[sortKey]) return 1 * modifier;
-            return 0;
-          });
-        }
-        return todoData_tmp;
-      },
+  },
+  computed: {
+    sortedUsers() {
+      const todoData_tmp = [...this.todoData];
+      const sortKey = this.sortKey;
+      const sortOrder = this.sortOrder;
+
+      if (sortKey) {
+        todoData_tmp.sort((a, b) => {
+          const modifier = sortOrder === 1 ? 1 : -1;
+          if (a[sortKey] < b[sortKey]) return -1 * modifier;
+          if (a[sortKey] > b[sortKey]) return 1 * modifier;
+          return 0;
+        });
+      }
+      return todoData_tmp;
     },
-    methods: {
-      sortBy(key) {
-        if (this.sortKey === key) {
-          this.sortOrder = this.sortOrder * -1;
-        } else {
-          this.sortKey = key;
-          this.sortOrder = 1;
-        }
-      },
+  },
+  methods: {
+    sortBy(key) {
+      if (this.sortKey === key) {
+        this.sortOrder = this.sortOrder * -1;
+      } else {
+        this.sortKey = key;
+        this.sortOrder = 1;
+      }
     },
-  }
+  },
+}
 </script>
 
 <style scoped>
-table{
+table {
   border-collapse: collapse;
   border-spacing: 0;
   position: relative;
@@ -94,33 +128,34 @@ table{
   width: 80%;
 }
 
-table tr{
+table tr {
   border-bottom: solid 1px #eee;
   cursor: pointer;
 }
 
-table tr:hover{
+table tr:hover {
   background-color: #d4f0fd;
 }
 
-table th,table td{
+table th,
+table td {
   text-align: center;
   width: 25%;
   padding: 15px 0;
 }
 
-table td.icon{
+table td.icon {
   background-size: 35px;
   background-position: left 5px center;
   background-repeat: no-repeat;
   padding-left: 30px;
 }
 
-table th{
+table th {
   background-color: #f2f2f2;
 }
 
-img{
+img {
   width: 40px;
   height: 40px;
 }
@@ -131,6 +166,11 @@ img{
 
 .sort-desc:after {
   content: ' ▼';
+}
+
+.dangerClass {
+  background-color: #c6f118;
+
 }
 
 /* table{
